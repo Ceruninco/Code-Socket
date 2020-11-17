e6cdab0ab3ad53da809b3aa7ebc9b988390cf2d8
Gheorghe Tutunaru <tutunaru.gheorghe111@gmail.com>
2020-11-17 08:45:01 +0100

    3rd commit

--- src/stream/ClientThread.java
+++ src/stream/ClientThread.java
@@ -12,65 +12,65 @@ import java.net.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ClientThread
-	extends Thread {
-	
+		extends Thread {
+
 	private Socket clientSocket;
 
 	/**
-	* Id of the client which will be the key in the map <code>activeClients</code>
-	*/
+	 * Id of the client which will be the key in the map <code>activeClients</code>
+	 */
 	static int idClient = 0;
 
 	/**
 	 * Map of active clients connected to the chat
 	 * (using this map to not bother with synchronisation)
- 	 */
+	 */
 	static ConcurrentHashMap<Integer, Socket> activeClients = new ConcurrentHashMap<Integer, Socket>();;
-	
+
 	ClientThread(Socket s) {
 		this.clientSocket = s;
 	}
 
- 	/**
-  	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
-  	**/
+	/**
+	 * receives a request from client then sends an echo to the client
+	 * @param clientSocket the client socket
+	 **/
 	public void run() {
-    	  try {
-    	  	  BufferedReader socIn = null;
-    		  socIn = new BufferedReader(
-    			new InputStreamReader(clientSocket.getInputStream()));    
-    		  PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
-    		  BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			BufferedReader socIn = null;
+			socIn = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
+			PrintStream socOut = new PrintStream(clientSocket.getOutputStream());
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-    		  // add client socket to the map of connected clients
-			  activeClients.put(idClient, clientSocket);
-			  idClient++;
-
-
-			  while (true) {
+			// add client socket to the map of connected clients
+			activeClients.put(idClient, clientSocket);
+			idClient++;
 
 
-				    //String l = stdIn.readLine();
-					//socOut.println(l);
-
-    		    	String line = socIn.readLine();
-    		    	//socOut.println(line);
+			while (true) {
 
 
-					// broadcast message to all available clients
-				    for(int clientHost : activeClients.keySet()) {
-					  	// get each socket here and send a message to them.
-						PrintStream clientOut = new PrintStream(activeClients.get(clientHost).getOutputStream());
-						clientOut.println(line);
-						System.out.println(activeClients.get(clientHost).getInetAddress().getHostName() + " " + activeClients.size());
-				    }
-    		  }
-    	  } catch (Exception e) {
-    	  	  System.err.println("Error in EchoServer:" + e);
-          }
+				//String l = stdIn.readLine();
+				//socOut.println(l);
+
+				String line = socIn.readLine();
+				//socOut.println(line);
+
+
+				// broadcast message to all available clients
+				for(int clientHost : activeClients.keySet()) {
+					// get each socket here and send a message to them.
+					PrintStream clientOut = new PrintStream(activeClients.get(clientHost).getOutputStream());
+					clientOut.println(line);
+					System.out.println(activeClients.get(clientHost).getInetAddress().getHostName() + " " + activeClients.size());
+				}
+			}
+		} catch (Exception e) {
+			System.err.println("Error in EchoServer:" + e);
+		}
 	}
-  
+
 }
 
-  
+
