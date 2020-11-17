2a39491a82f8ba8b992e9842be887999d62474f3
Julien Nicolas <julien.nicolas88@protonmail.com>
2020-11-17 09:43:01 +0100

    multithread UDP

--- src/stream/UDPEchoClient.java
+++ src/stream/UDPEchoClient.java
@@ -6,12 +6,15 @@
  */
 package stream;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
-
-public class EchoClient {
+public class UDPEchoClient {
 
  
   /**
